900bb38ed7bd930c0d7c64335f4a571f9f818c15
Alisson <alissonbusiness2024@gmail.com>
2025-04-13 13:47:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,31 +7,31 @@ const temaSalvo = localStorage.getItem("tema");
 
 if (temaSalvo === "dark") {
   body.classList.add("dark-mode");
-  icone.src = "img/Sol.png";
-  iconeGit.src = "img/github-Light.png";
-  iconeLink.src = "img/linkedin-Light.png";
-  iconeInta.src = "img/instagram-Light.png";
+  icone.src = "./img/Sol.png";
+  iconeGit.src = "./img/github-Light.png";
+  iconeLink.src = "./img/linkedin-Light.png";
+  iconeInta.src = "./img/instagram-Light.png";
 } else {
-    icone.src = "img/Lua.png"; 
-    iconeGit.src = "img/github-Dark.png";
-    iconeLink.src = "img/linkedin-Dark.png";
-    iconeInta.src = "img/instagram-Dark.png";
+    icone.src = "./img/Lua.png"; 
+    iconeGit.src = "./img/github-Dark.png";
+    iconeLink.src = "./img/linkedin-Dark.png";
+    iconeInta.src = "./img/instagram-Dark.png";
 }
 
 icone.addEventListener("click", () => {
   body.classList.toggle("dark-mode");
 
   if (body.classList.contains("dark-mode")) {
-    icone.src = "img/Sol.png";
-    iconeGit.src = "img/github-Light.png";
-    iconeLink.src = "img/linkedin-Light.png";
-    iconeInta.src = "img/instagram-Light.png";
+    icone.src = "./img/Sol.png";
+    iconeGit.src = "./img/github-Light.png";
+    iconeLink.src = "./img/linkedin-Light.png";
+    iconeInta.src = "./img/instagram-Light.png";
     localStorage.setItem("tema", "dark");
   } else {
-    icone.src = "img/Lua.png";
-    iconeGit.src = "img/github-Dark.png";
-    iconeLink.src = "img/linkedin-Dark.png";
-    iconeInta.src = "img/instagram-Dark.png";
+    icone.src = "./img/Lua.png";
+    iconeGit.src = "./img/github-Dark.png";
+    iconeLink.src = "./img/linkedin-Dark.png";
+    iconeInta.src = "./img/instagram-Dark.png";
     localStorage.setItem("tema", "light");
   }
 });
